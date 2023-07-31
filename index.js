36cc76530f0d561db3d7081d44336e43830f856f
Poly Akhtar <poly09876pm@gmail.com>
2023-07-31 23:29:32 +0600

    all api set up

--- index.js
+++ index.js
@@ -56,11 +56,11 @@ async function run() {
       res.send(result);
     });
 
-    app.post('/users', async (req, res) => {
+    app.post("/users", async (req, res) => {
       const user = req.body;
-      const result = await usersCollection.insertOne(user);
-      res.send(result);
-    });
+      const result=await usersCollection.insertOne(user);
+      res.send(result)
+  });
 
     app.get('/users/admin/:email', async (req, res) => {
       const email = req.params.email;
